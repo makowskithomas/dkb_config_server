675c5855f3196027d88e376519784f3b4bd22c30
mos <mos.cheele+github@gmail.com>
2021-06-09 16:54:48 +0200

    Initial commit

---
+++ src/main/java/de/moscon/dkb_config_server/DkbConfigServerApplication.java
@@ -0,0 +1,13 @@
+package de.moscon.dkb_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkbConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkbConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/moscon/dkb_config_server/DkbConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.moscon.dkb_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkbConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
