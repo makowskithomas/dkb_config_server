82171d93ef3ae2550c92890daa9f675e028864c6
mos <mos.cheele+github@gmail.com>
2021-06-09 17:46:12 +0200

    configure config-server

--- src/main/java/de/moscon/dkb_config_server/DkbConfigServerApplication.java
+++ src/main/java/de/moscon/dkb_config_server/DkbConfigServerApplication.java
@@ -2,8 +2,17 @@ package de.moscon.dkb_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/*
+   Check/See configuration with:
+
+    http://localhost:8888/application/default
+
+ */
 
 @SpringBootApplication
+@EnableConfigServer
 public class DkbConfigServerApplication {
 
 	public static void main(String[] args) {
